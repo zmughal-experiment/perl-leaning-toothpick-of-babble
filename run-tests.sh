0943deb9cb17273f875178f8f41d904d52558ac8
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-08-16 18:50:12 -0400

    Use find to run tests in each directory

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,6 @@
 
 CURDIR=`dirname "$0"`
 
-for dist_module in Dist::Zilla App::Cmd Getopt::Long::Descriptive; do
-	dist=$(echo $dist_module | sed 's/::/-/g' )
-	( cd $dist ; if [ -d lib ]; then export PERL5LIB=$(pwd)/lib; fi; yath -j4 -q )
-done
+find work/ -mindepth 1 -maxdepth 1 -type d -print -exec sh -c '
+	cd {} ; if [ -d lib ]; then export PERL5LIB=$(pwd)/lib; fi; yath -j4 -q
+' \;
