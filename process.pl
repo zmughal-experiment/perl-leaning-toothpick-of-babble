65781c8975d56d2aa863a00b62ccd893d7de6985
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-11-17 21:11:20 -0500

    Converting main part of script from Bash to Perl

--- process.pl
+++ process.pl
@@ -24,58 +24,94 @@ package Dist {
 	use Mu;
 	use Module::Runtime qw(is_module_name);
 	use Mojo::UserAgent;
-	use feature qw(signatures postderef);
-	no warnings "experimental::signatures";
 
 	lazy ua => sub { Mojo::UserAgent->new };
 
 	ro 'main_module_name';
 	rw 'version' => ( required => 0 , predicate => 1 );
 
-	rw [qw(url)] => ( required => 0  );
+	lazy url => sub {
+		my ($self) = @_;
+		$self->_dist_info->{download_url};
+	};
 
-	sub BUILD($self, @) {
-		die "Not a valid module name" unless is_module_name($self->main_module_name);
-
+	lazy _dist_info => sub {
+		my ($self) = @_;
 		my $METACPAN_DOWNLOAD_URL_BASE = "https://fastapi.metacpan.org/v1/download_url/";
 		my $dist_info = $self->ua->get(
 			"$METACPAN_DOWNLOAD_URL_BASE/@{[ $self->main_module_name ]}"
 			. ($self->has_version ? "?version===@{[ $self->version ]}" : '')
 
 		)->result->json;
-
-		$self->url( $dist_info->{download_url} );
 		$self->version( $dist_info->{version} ) unless $self->has_version;
+		$dist_info;
+	};
+
+	sub BUILD {
+		my ($self) = @_;
+		die "Not a valid module name" unless is_module_name($self->main_module_name);
 	}
 };
 
 package Process {
 	use Mu;
-	use feature qw(signatures postderef);
-	no warnings "experimental::signatures";
 	use PerlX::Maybe;
 	use Env qw(@PERL5LIB);
+	use Module::Runtime qw(use_module);
 
 	use Config::INI::Reader::Multiline;
 	use File::Find::Rule::Perl;
+	use Path::Tiny;
+
+	use Babble::Grammar;
+
+	use File::Which;
+	use Pod::Strip;
+
+	use autodie qw(:all);
+	use IPC::System::Simple;
+	use File::chdir;
+	use File::Find::Rule       ();
+	use File::Find::Rule::Perl ();
+
+	use MCE;
+	use curry;
+	use Term::ProgressBar;
+	use Babble::PluginChain;
 
 	ro 'config_path';
 
 	lazy work_dir => sub { path($FindBin::Bin, 'work') };
 
-	sub fetch_or_reset($self, $dist) {
+	sub fetch_or_reset {
+		my ($self, $dist) = @_;
 	}
 
-	sub run($self) {
+	sub get_plugin {
+		my ($plugin_name, $grammar) = @_;
+		$grammar ||= Babble::Grammar->new;
+		$plugin_name =~ s/^::/Babble::Plugin::/;
+		my $p = use_module($plugin_name)->new;
+		$p->extend_grammar($grammar) if $p->can('extend_grammar');
+		$p;
+	}
+
+	sub run {
+		my ($self) = @_;
 		my $config = Config::INI::Reader::Multiline
 			->read_file($self->config_path);
 		my @dists;
-		while( my ($module, $data) = each %$config ) {
+		for my $module (sort keys %$config) {
+			next if $module =~ /^-/;
+			my $data = $config->{$module};
+
 			push @dists, Dist->new(
 				main_module_name => $module,
 				maybe version => $data->{version},
 			);
 		}
+
+		my @plugins =  sort keys %{ $config->{-plugins=>} };
 
 		my $babble_filter = <<'BASH';
 BABBLE_FILTER() {
@@ -90,81 +126,144 @@ BASH
 		do {
 		local $ENV{PERL5LIB} = $ENV{PERL5LIB};
 		unshift @PERL5LIB, @main::VENDOR_LIB;
+		die "Need git" unless which('git');
+		die "Need curl" unless which('curl');
+		die "Need make" unless which('make');
+		die "Need tar with support for --strip-components (e.g., GNU tar, bsdtar)" unless which('tar');
+
+		my $rule =  File::Find::Rule->or(
+			File::Find::Rule::Perl->perl_module,
+			File::Find::Rule::Perl->perl_script,
+		);
+		my $all_perl_rule = File::Find::Rule::Perl->perl_file;
+		my $eumm_rule = File::Find::Rule->file->name( 'Makefile.PL' );
+
+		my %plugin_cache;
+		my $grammar = Babble::Grammar->new;
+		@plugin_cache{@plugins} = map get_plugin($_, $grammar), @plugins;
+		my $blank_top = $grammar->match(Document => '');
+		$_->transform_to_plain($blank_top) for values %plugin_cache;
+
+		my $mce = MCE->new(
+			max_workers => 4,
+			#chunk_size => 64,
+		);
 
 		for my $dist (@dists) {
 			local $ENV{TARBALL_URL} = $dist->url;
-			local $ENV{dist_module} = $dist->main_module_name;
+			local $ENV{dist_module} = my $dist_module = $dist->main_module_name;
 
-			system(qw(bash -c), $babble_filter . <<'BASH');
-	#set -eoxu pipefail
-	set -eou pipefail
+			my $dist_dir = $self->work_dir->child(
+				$dist->main_module_name =~ s/::/-/gr
+			);
+			local $ENV{dist_dir} = "$dist_dir";
 
-# requires
-# git, perl, sponge, find, sed, xargs, parallel
-# cpanm Babble Pod::Strip
-#
-# tar with support for --strip-components (e.g., GNU tar, bsdtar)
+			{
+				my $TAG = 'base';
+				if( ! -d $dist_dir ) {
+					print "Downloading @{[ $dist->url ]}\n";
+					system(qw(git init -q), $dist_dir);
+					system 'curl -s $TARBALL_URL | tar -C $dist_dir --strip-components 1 -xzf -';
+					{
+						local $CWD = "$dist_dir";
+						system qw(git add --all --force .);
+						system qw(git commit -q -m), "initial import of $dist_module";
+						system qw(git tag), $TAG;
+					}
+				} else {
+					if( -f $dist_dir->child('Makefile') ) {
+						system qw(make -C), $dist_dir, qw(clean);
+					}
+					{
+						local $CWD = "$dist_dir";
+						system qw(git checkout main);
+						system qw(git reset --hard), $TAG;
+					}
+				}
+			}
 
-	dist=work/$(echo $dist_module | sed 's/::/-/g; s/@.*$//' )
+			my @FIND_FILES = $rule->in( $dist_dir );
+			my @ALL_PERL_FILES = $all_perl_rule->in($dist_dir);
+			my @EUMM_FILES = $eumm_rule->in($dist_dir);
 
-	TAG='base';
-	if [ ! -d $dist ]; then
-		git init -q $dist
-		curl -s $TARBALL_URL | tar -C $dist --strip-components 1 -xzf -
-		git -C $dist add --all --force .
-		git -C $dist commit -q -m "initial import of $dist_module"
-		git -C $dist tag $TAG
-	else
-		if [ -f $dist/Makefile ]; then make -C $dist clean; fi
-		git -C $dist checkout main
-		git -C $dist reset --hard $TAG
-	fi
+			{
+				# Removing POD
+				my $TAG = 'pod';
+				for my $file (@FIND_FILES) {
+					next if $file =~ m,/t/|/corpus/,;
+					path($file)->edit(sub {
+						s/^ \#pod \N*? \n//xmsg;
+						my $p = Pod::Strip->new;
+						my $podless;
+						$p->output_string( \$podless );
+						$p->parse_string_document( $_ );
+						$_ = $podless;
+					});
+				}
+				{
+					local $CWD = "$dist_dir";
+					system qw(git add .);
+					system qw(git commit -q -m), 'Remove POD', '--allow-empty';
+					system qw(git tag -f), $TAG;
+				}
+			}
 
-	FIND_FILES='\( \( -type f -name "*.pm" \) -o \( -type f -regex ".*/bin/.*" -exec grep -qP "^#!.*perl$" {} \; \) \)'
-	if true; then
-		echo -n "Removing POD..."
-		TAG='pod'
-		eval "find $dist $FIND_FILES -print" | grep -vP '/t/|/corpus/' | xargs sed -i '/^#pod/d'
-		eval "find $dist $FIND_FILES -print" | grep -vP '/t/|/corpus/' | xargs -I{} sh -c "perl -MPod::Strip -e 'Pod::Strip->filter(shift @ARGV)' {} | sponge {}"
-		git -C $dist add . && git -C $dist commit -q -m 'Remove POD' --allow-empty
-		git -C $dist tag -f $TAG
-	fi
+			{
+				# Removing Perl version...
+				my $TAG = 'perl-version';
+				for my $file (@ALL_PERL_FILES) {
+					next if $file =~ m,/corpus/,;
+					path($file)->edit_lines(sub { s/^use \s+ v?5[.0-9]* \s* ; $/#$&/x } );
+				}
+				for my $file (@ALL_PERL_FILES) {
+					path($file)->edit_lines(sub { s/^ no \s+ feature \s+ 'switch'; \s* $/#$&/x } );
+				}
+				for my $file (@EUMM_FILES) {
+					path($file)->edit_lines(sub { s/^ .* MIN_PERL_VERSION .* $/#$&/x });
+				}
+				{
+					local $CWD = "$dist_dir";
+					system qw(git add .);
+					system qw(git commit -q -m), 'Remove explicit use Perl version / extra features', '--allow-empty';
+					system qw(git tag -f), $TAG;
+				}
+			}
 
-	if true; then
-		echo -n "Removing Perl version..."
-		TAG='perl-version'
-		find $dist -type f | grep -v '/corpus/' | xargs perl -pi -e 's/^use \s+ v?5[.0-9]* \s* ; $/#$&/x'
-		find $dist -type f | xargs perl -pi -e "s/^ no \s+ feature \s+ 'switch'; \s+ $/#$&/x"
-		find $dist -type f -name 'Makefile.PL' | xargs perl -pi -e 's/^ .* MIN_PERL_VERSION .* $/#$&/x'
-		git -C $dist add . && git -C $dist commit -q -m 'Remove explicit use Perl version / extra features' --allow-empty
-		git -C $dist tag -f $TAG
-	fi
-
-	for plugin in \
-			PackageBlock PackageVersion \
-			DefinedOr \
-			PostfixDeref \
-			State SubstituteAndReturn \
-			Ellipsis \
-		; do
-		TAG="babble-$plugin"
-		TIMING_TEMP=$(mktemp BABBLE_TIMING_XXXXXX)
-		/usr/bin/time -p -o $TIMING_TEMP bash -c "
-			find $dist $FIND_FILES -print \
-				| grep -v '/corpus/' \
-				| parallel -j4 -N1 --bar -X \
-					'BABBLE_FILTER ::$plugin {}'
-			"
-		git -C $dist add . && git -C $dist commit -q -m "Apply Babble plugin $plugin" -m "$(cat $TIMING_TEMP)" --allow-empty
-		git -C $dist tag -f $TAG | tr -d '\n'; echo -n "...";
-		git -P -C $dist show --format= --shortstat $TAG
-		rm $TIMING_TEMP
-	done
-
-BASH
+			for my $plugin ( @plugins ) {
+				my $TAG = "babble-@{[ $plugin =~ s/::/-/gr ]}";
+				my $progress_bar = Term::ProgressBar->new({
+					count => 0+@ALL_PERL_FILES,
+					name  => $plugin,
+				});
+				my $pc;
+				$mce->foreach(\@ALL_PERL_FILES,
+					{
+						progress => $progress_bar->curry::update,
+						user_begin => sub {
+							my $p = $plugin_cache{$plugin};
+							$pc = Babble::PluginChain->new( plugins => [ $p ], grammar => $grammar );
+						},
+					},
+					sub {
+						my ($mce, $chunk_ref, $chunk_id) = @_;
+						return if $chunk_ref->[0] =~ m,/corpus/,;
+						my $file = path($chunk_ref->[0]);
+						$file->edit(sub {
+							$_ = $pc->transform_document($_);
+						});
+					}
+				);
+				{
+					local $CWD = "$dist_dir";
+					system qw(git add .);
+					system qw(git commit -q -m), "Apply Babble plugin $plugin", '--allow-empty';
+					system qw(git tag -f), $TAG;
+					system qw(git -P show --format= --shortstat), $TAG;
+				}
+			}
 		}
 
-		system(qw(bash -c), $babble_filter . <<'BASH');
+		system(qw(bash -c), $babble_filter . <<'BASH') if 1;
 
 perl -pi -e 's/"feature" =>/#$&/' work/Dist-Zilla/Makefile.PL
 BABBLE_FILTER ::DefinedOr,::PostfixDeref  work/Dist-Zilla/t/plugins/archive_builder.t
