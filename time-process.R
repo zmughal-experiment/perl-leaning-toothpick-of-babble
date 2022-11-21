ea0627a8899e3c606d1321fb6aceecb3b5fd2643
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-11-21 17:44:34 -0500

    Update analysis

--- time-process.R
+++ time-process.R
@@ -3,14 +3,17 @@ data <- fromJSON('timing.json')
 
 data$run <- as.factor(data$run)
 data$version <- as.factor(data$version)
+data$workers <- as.factor(data$workers)
 
-data <- subset( data, select = c('run', 'version', 'elapsed', 'cache', 'match_pos_cache', 'warm_cache'))
+data <- subset( data, select = -c(timing))
+
+model <- elapsed ~ version + workers * ( cache * match_pos_cache * warm_cache + bail_out_early + bail_out_late )
 
 attach(data)
-agg <- aggregate( elapsed ~ cache * match_pos_cache * warm_cache + version, run, mean )
+agg <- aggregate(model, run, mean )
 detach(data)
 
-agg.aov <- subset(agg, version == 'perl-5.34.0@babble'  )
-print(agg.aov)
-a <- aov( elapsed ~ cache * (match_pos_cache + warm_cache) , agg.aov )
-print(a); print( summary(a) )
+agg.perl534 <- subset(agg, version == 'perl-5.34.0@babble'  )
+print(agg.perl534)
+fit <- aov( model, agg.perl534 )
+print(fit); print( summary(fit) )
